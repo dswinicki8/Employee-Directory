4bfe6690f6da56c2ff373a926298daa5b00bd901
Drew Swinicki <dswinicki93@gmail.com>
2020-08-12 19:17:54 -0400

    updated serach form from dog breed activity for app

--- employee-directory/src/components/searchForm.js
+++ employee-directory/src/components/searchForm.js
@@ -6,22 +6,18 @@ function SearchForm(props) {
   return (
     <form className="search">
       <div className="form-group">
-        <label htmlFor="employees">Sort Employees By:</label>
+        <label htmlFor="employees">Search Employees By:</label>
         <input
           value={props.search}
           onChange={props.handleInputChange}
-          name="breed"
+          name="search"
           list="breeds"
           type="text"
           className="form-control"
-          placeholder="Type in a dog breed to begin"
-          id="breed"
+          placeholder="Search Employees"
+          id="search"
         />
-        <datalist id="breeds">
-          {props.breeds.map(breed => (
-            <option value={breed} key={breed} />
-          ))}
-        </datalist>
+        
         <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
           Search
         </button>

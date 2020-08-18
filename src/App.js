01dc5cdf7b93feff74a71fe390e70129032604e4
Drew Swinicki <dswinicki93@gmail.com>
2020-08-18 19:56:41 -0400

    changed file structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import SearchEmployee from './components/searchEmployee';
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      
+
+       <SearchEmployee/>
+      
+    </div>
+  );
+}
+
+export default App;

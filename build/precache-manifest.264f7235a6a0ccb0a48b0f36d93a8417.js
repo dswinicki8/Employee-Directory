b0f5efc0b5c94465255f47fb5b99f5426d6bf2b0
Drew Swinicki <dswinicki93@gmail.com>
2020-08-18 20:13:17 -0400

    deploy

---
+++ build/precache-manifest.264f7235a6a0ccb0a48b0f36d93a8417.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201f077174060fab9e526de635dbbd85",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "b13e6739573e87306547",
+    "url": "/employee-directory/static/js/2.94725154.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.94725154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "894c6b514d55de9e39f3",
+    "url": "/employee-directory/static/js/main.8703e479.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import SearchEmployee from './components/searchEmployee';
-import "./App.css";
+
 
 function App() {
   return (
